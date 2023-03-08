e206343891348d7bad80f6909da878415bcb7030
Zin Min <zinminaungzm.1998@gmail.com>
2023-03-08 17:12:13 +0630

    'commit'

---
+++ DataEncription/Encryption/DataEnctyptionService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEncription.Encryption
+{
+   public class DataEnctyptionService
+    {
+        //Encrypt with Aes
+        public static byte[] EnctyptWithAes(string key, string password)
+        {
+            byte[] datas ;
+            byte[] iv = new byte[16];
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                //aes.Padding = PaddingMode.None;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(aes.Key,aes.IV), CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(password);
+                        }
+                    }
+                    datas = ms.ToArray();
+                }
+            }
+            return datas;
+        }
+        //Dectypt with Aes
+        public  static string DecryptwithAes(string key , byte[] enctyptedData)
+        {
+            string data = string.Empty;
+            byte[] iv = new byte[16];
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                aes.Padding = PaddingMode.None;
+                using (MemoryStream ms = new MemoryStream(enctyptedData))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, aes.IV), CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                         data=  sr.ReadToEnd();
+
+                         data=data.Replace("\0", string.Empty).
+                                            Replace("\b", string.Empty).
+                                            Replace("\v", string.Empty).
+                                            Replace("\t", string.Empty).
+                                            Replace("\u0010", string.Empty).
+                                            Replace("\u0003", string.Empty).
+                                            Replace("\u0006", string.Empty);
+                        }
+                    }
+                   
+                }
+            }
+            return data;
+        }
+    }
+}
--- DataEncription/Program.cs
+++ DataEncription/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using DataEncription.Encryption;
 
 namespace DataEncription
 {
     class Program
     {
+       // .NET Core provides various encryption methods that can be used to protect data and ensure its confidentiality.
         static void Main(string[] args)
         {
+            string decryptedData = string.Empty;
+            string key = "b14ca5898a4e4133bbce2ea2315a1916";
+            string password = Console.ReadLine();
+            byte[] data = DataEnctyptionService.EnctyptWithAes(key,password);
+            // StoreBase64StringInDatabase
+            Console.WriteLine(Convert.ToBase64String(data));
+            //
+            if (data != null)
+            {
+                decryptedData = DataEnctyptionService.DecryptwithAes(key, data);
+                Console.WriteLine(decryptedData);
+            }
+            //if true print equal
+            if (password.Equals(decryptedData))
+            {
+                Console.WriteLine("equals");
+            }
         }
     }
+    
 }
